467bbd96107a7c25d41e878ed62943276b6c994b
dylorr <34012156+dylorr@users.noreply.github.com>
2021-06-16 12:38:37 -0400

    add favicon, change wording

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,9 +11,8 @@ function Home() {
           <Container className="home-content" style={{ marginBottom: '20%'}}>
             <Row>
               <Col md={{span:10, offset:1}} className="home-header text-center">
-
                 <h1 style={{ textAlign: 'center'}} className="heading">
-                  All your <span className="green">nft</span> news, consolidated in one place.
+                  All your <span className="green">nft</span> news now lives in one space.
                 </h1>
                 <h6 style={{ paddingBottom: 15 }} className="subtitle-home">
                   Daily digest, project alerts and informational updates.
