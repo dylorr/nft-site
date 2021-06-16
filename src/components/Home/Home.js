84fe563a1cfdec5f5abc0da83c5bfee487508cc7
dylorr <34012156+dylorr@users.noreply.github.com>
2021-06-16 13:20:54 -0400

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,10 +12,10 @@ function Home() {
             <Row>
               <Col md={{span:10, offset:1}} className="home-header text-center">
                 <h1 style={{ textAlign: 'center'}} className="heading">
-                  All your <span className="green">nft</span> news now lives in one space.
+                  All your <span className="green">nft</span> news, contextualized.
                 </h1>
                 <h6 style={{ paddingBottom: 15 }} className="subtitle-home">
-                  Daily digest, project alerts and informational updates.
+                  Daily digests, project alerts and due diligence.
                 </h6>
                 <Button variant='outline-dark' className=" btn btn-discord pulse" href="https://discord.com/invite/tEAMRKSKUX">Join Discord </Button>
               </Col>
